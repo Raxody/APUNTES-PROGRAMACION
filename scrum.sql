3d732c0797e85b2b5cffd3d57a199308c2b6f810
andres.lopez <andres.lopez@ceiba.com.co>
2022-05-10 16:48:53 -0500

    Apuntes scrum

---
+++ scrum.sql
@@ -0,0 +1,72 @@
+Sprint: Es un tiempo, máximo 4 semanas, minimo una semana
+
+ SCRUM: marco de trabaajo agil
+
+ Agilidad: Ser vivo, perspicaz y saber encontrar la forma de hacer las cosas. Capacidad de adaptarnos, acostumbrarnos a hacer algo
+
+ roles: product owner, scrum master y desarrolladores
+
+Daily: Es decir que vamos  a hacer hoy solo en 15 minutos y todos los dias
+
+Sincronizacion: Todos conversan, con las reglas que deseen y los dias que sea 
+
+Reunión de seguimiento: es mirar el avance
+
+sprintreview: es un espacio de retroalimentacion driario y mirar que si se cumplioo todo, solo le interesa el prodducto
+
+retrospectiva: resolvemos asuntos asociados al proceso, como nos fue?, como nos comunicamos?, hubo sobre esfuerzos?
+
+scrum master: es un experto en scrum y además encarga trabajos, velocidad del equipo, capacidad del equipo
+
+product owner: el que dice que quiere en el proyecto, como va, usabilidad, retorno de inversion
+
+Backlog: tiene todo pa construir el producto, lo tecnico, riesgos, costos
+
+Refinamiento: dura el 10% del sprint, se trabaja en equipo, 
+
+----------------TRES ARTEFACTOS
+
+Product Backlog: Es una lista ordenada de todos los requerimientos, todos los modulos, regulacion de ley, etc
+
+Sprint Backlog: Es un plan de trabajo, osea traigo una parte de un sprint 
+
+Incremeneto: El resultado del equipo de trabajo, osea las sumas de los elementos que perminten construir valor
+
+
+definition of done: como entrego y en que condiciones entrego 
+
+
+historias de usuario: Son los requerimientos de usuario 
+
+Feature:  hablan especificamente de caracteristicas de la funcion osea se segmentan
+
+historia de usuario: son tareas diaras para terminar un sprint
+
+- las 3c de las historia de usuario 
+
+    -card: es lo que se quiere como usuario, " yo como usuario quiero registrarme a traves de un formulario"
+
+    -conversation: son los criterios de aceptacion, 
+
+    -confirmation: 
+
+
+    - invest:
+
+i -> independiente: no dependa una historia de otra
+n -> negociable: Agregar detalles a las historias
+v -> valor: que den valor al negocio
+e -> estimable: que se puedene estimar
+s -> pequeño: que sea pequeña
+t -> Testeable: qie se pueda probar
+
+
+DOR -> definicion de iniciar  - definition of ready 
+
+Es un chequeo de rutina para hacer las cosas a ver si se esta cumpliendo lo de las historias de usuario
+
+DOD -> definicion de terminado 
+
+Es lo que cumplimos o lo que termianmos
+
+uno es antes y el otro es despues
