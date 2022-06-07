e07de43266f8dd62cd0d1d3e2dce9dc6ea188404
andres.lopez <andres.lopez@ceiba.com.co>
2022-06-07 10:42:54 -0500

    Avanzando front

---
+++ repasoConceptosARUS.sql
@@ -0,0 +1,75 @@
+-- ¿Que es la nube o cloud?
+    La nube son sistemas ordenadores o computadores que se encuentran desde otro 
+    lugar en los cuales son accesibles en cualquier momento ya que manejan
+    servidores de fondo y siempre estan funcionando estos manejan todo desde alla
+    a la par que su internet tiene muchas capas o seguridades por si se cae una 
+    entra otra y así 
+    -- Cloud publica
+      Que todos la pueden tener es como accesible a cualquiera y la comparten todos
+    -- Cloud privata
+        Cuando toda la infraestructura es para mi osea es personal, nadie más puede hacer
+     uso de esta
+    -- Cloud hibrida 
+    Pueden haber partes privadas y partes publicas dejando lo que no es tan critico
+    para que cualquiera la pueda usar
+
+-- Que son los modelos cloud
+    --Modelo de distribucion 
+        --As a service aaS
+            servicio o producto a traves de internet pago o gratiuito y que se
+             puede redimensionar segun las necesidades del cliente
+    --IaaS - Infraestructura como servicio
+    Cuando se contratan y alquilo equipos informaticos con plataforma de virtualizacion
+    con todos los poderes y esto se encuentra con el rpvoeedor el es el que se el
+    encarga de proporcionar todo y aca uno tiene que cuadrar la infraestructura
+    -- PaaS - plataforma como servicio
+    Dedicada a desarrolladores o empresas de desarrolladores cuando entregan 
+    infraestructura con funcionamiento ya realizado y ya uno solo la debe usar
+    o trabajar sobre lo que le dan a uno sobre ella
+    -- Saas - Software como servicio
+    Servicio mas usado, cualquier servicio en la web para uso inmediato
+    no se debe insatalar nada, ni mover nada lo unico es usarlo 
+
+-- ¿Que es la virtualizacion?
+es la representacion o imitacion de una computadora o servidor 
+
+-- Docker  -- Contenedores
+
+virtualizacion de contenedores
+entornos virtualizados aislados ahí se testea, programa usa y no hay peligro que se 
+dañe.
+
+Las imagenes en docker son como si fuesen unas plantillas,  se pueden encontrar dentro
+SO, librerias, archivos binarios, herramientas, otros tipos de archivos, etc. 
+
+Un contenedor es una copia de una imagen de docker -> entonces
+Copia de una imagen de docker + aplicacion desplegada en ella = instancia ejecutable.
+
+Estos contenedores se peuden trasladar de un pc a otro de un servidor a otro 
+despreocupandome de todo porque todo se trabaja sobre el contenedor que se traiga
+solo se debe tener instalado docker
+
+Las imagenes en docker no dependen de los contenedores
+
+Los contenedores en docker si dependen de las imagenes en docker 
+
+
+-- Kubernetes  K8s
+
+Esto es una unica plataforma que permite gestionar muchos contenedores
+
+Ayuda en la automatizacion del sistemas, realiza despliegues facilmente, ayuda
+a escalar o aumentar la capacidad del entorno, distribuye en varios nodos.
+
+Controla muchos contenedores en muchos nodos 
+
+--Orquestador
+
+ayuda a gestionar y coordinerar diferentes sistemas de procesamiento, almacenamiento,
+Software
+
+
+
+
+
+
